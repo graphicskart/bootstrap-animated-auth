b1ade33296f13ea7f60946f04d64c1264f720735
graphicskart <graphicskart2018@gmail.com>
2019-03-02 14:36:41 +0530

    Updates

---
+++ precache-manifest.2aa612e085e359580f513f75ee254a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33262fd71e368441bfb8",
+    "url": "/bootstrap-animated-auth/static/css/main.d6d04daf.chunk.css"
+  },
+  {
+    "revision": "33262fd71e368441bfb8",
+    "url": "/bootstrap-animated-auth/static/js/main.33262fd7.chunk.js"
+  },
+  {
+    "revision": "8cbd242fda1dcb14eb9f",
+    "url": "/bootstrap-animated-auth/static/js/runtime~main.8cbd242f.js"
+  },
+  {
+    "revision": "80ad22b7e2bfb846e460",
+    "url": "/bootstrap-animated-auth/static/js/2.80ad22b7.chunk.js"
+  },
+  {
+    "revision": "68d8029ff129e615165a4a255337c49f",
+    "url": "/bootstrap-animated-auth/index.html"
+  }
+];
